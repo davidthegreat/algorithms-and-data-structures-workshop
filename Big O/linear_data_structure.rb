b7b31f90c18cafa79a3c1d9815736949780ae45d
Iuliia Kotlenko <IuliiaKot@users.noreply.github.com>
2016-09-07 10:11:35 -0400

    Update linear_data_structure.rb

--- Big O/linear_data_structure.rb
+++ Big O/linear_data_structure.rb
@@ -3,6 +3,6 @@
 
 2. The ( and ) parenthesis need to be balanced in programming language. (5+6)*(7+8)/(4+3) is a valid expression because the parenthesis are properly matched, but ())) is not a valid expression. Write a balanced_parenthesis? that takes a string and returns true/false if the parenthesis are balanced. Use a stack to solve the problem!
 
-`example:` balanced_parenthesis?('((()))') should return true
+example: balanced_parenthesis?('((()))') should return true
 
-`example:` balanced_parenthesis?('(()') should return false
+example:  balanced_parenthesis?('(()') should return false
